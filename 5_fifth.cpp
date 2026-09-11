1c1304d59ffa3879b2b046f699e698c897b89679
dreamyginger <ginup08@gmail.com>
2026-09-11 18:57:21 +0200

    5th app - 1st change

--- 5_fifth.cpp
+++ 5_fifth.cpp
@@ -1,3 +1,33 @@
+/*
+PRENDERE IN INGRESSO 3 NUMERI INTERI TUTTI DIVERSI FRA LORO
+CALCOLARE IL MASSIMO TRA I 3 NUMERI E SCRIVERE TUTTI I DATI IN OUTPUT:
+"Primo numero: xxx"
+"Secondo numero: YYY"
+"Terzo numero: ZZZ"
+"Massimo: MAX"
+*/
+
 #include <iostream>
 using namespace std;
 
+int main() {
+    int x; int y; int z; int max;
+    cout << "Inserire il primo numero: ";
+    cin >> x;
+    cout << "Inserire il secondo numero: ";
+    cin >> y;
+    cout << "Inserire il terzo numero: ";
+    cin >> z;
+
+    if (x > y, x > z) {
+        x = max;
+    }
+    else if (y > x, y > z) {
+        y = max;
+    }
+    else {
+        z = max;
+    }
+
+    cout << "Primo numero: " << x << "\nSecondo numero: " << y << "\nTerzo Numero" << z << "\nMassimo:" << max;
+}
